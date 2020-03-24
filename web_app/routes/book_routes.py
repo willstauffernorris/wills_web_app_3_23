1fe68181a4fc82d09e21bdf191ce5268675401fa
willsn <willsn@gmail.com>
2020-03-24 10:04:44 -0600

    now you can add and view books

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, request, redirect
+
+from web_app.models import db, Book
 
 book_routes = Blueprint("book_routes", __name__)
 
@@ -6,23 +8,25 @@ book_routes = Blueprint("book_routes", __name__)
 @book_routes.route("/books.json")
 def list_books():
     print('visted books JSON page')
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
     return jsonify(books)
 
 
 @book_routes.route("/books")
 def books():
     print('visted books page')
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return render_template("books.html", books=books)
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    return render_template("books.html", books=book_records)
 
 @book_routes.route("/books/new")
 def new_book():
@@ -33,9 +37,14 @@ def new_book():
 def create_book():
     print("FORM DATA:", dict(request.form))
     # todo: store in database
-    return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
-        "book": dict(request.form)
-    })
+    #return jsonify({
+    #    "message": "BOOK CREATED OK (TODO)",
+    #    "book": dict(request.form)
+    #})
+
+    new_book = Book(title=request.form["book_title"], author_id=request.form['author_name'])
+    print(new_book)
+    db.session.add(new_book)
+    db.session.commit()
     #flash(f"Book '{new_book.title}' created successfully!", "success")
-    #return redirect(f"/books")
+    return redirect("/books")
