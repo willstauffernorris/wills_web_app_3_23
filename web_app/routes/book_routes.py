042b5feec735fa4e1d8b220f9092fd8a74cc7b15
willsn <willsn@gmail.com>
2020-03-24 07:30:53 -0600

    rendering an HTML template

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,16 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 book_routes = Blueprint("book_routes", __name__)
 
 
 @book_routes.route("/books.json")
 def list_books():
+    print('visted books JSON page')
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]
+    return jsonify(books)
+
+
+@book_routes.route("/books")
+def books():
     print('visted books page')
     books = [
         {"id": 1, "title": "Book 1"},
         {"id": 2, "title": "Book 2"},
         {"id": 3, "title": "Book 3"},
     ]
-
-
-    return jsonify(books)
+    return render_template("books.html")
